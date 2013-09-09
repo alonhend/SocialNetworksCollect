665bd820f9ae42362e5f3b78c7cd65ae30741fb0
Alon Hendelman <alon.hendelman@gmail.com>
2013-09-09 16:17:02 +0300

    Update Gemfile.lock for Heroku

---
+++ SocialCollection/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialCollection::Application.config.secret_token = '8fa675355bf4193fbdf6c0ae8f36d4c851818d42495caf0bd143fb21264b8f617bbd44544a411c4ae65e2001132852a2354041f22178ee9a49727af747d43e29'
